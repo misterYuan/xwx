9925fc37c1483375e4a6f83fdfe8a58e827a2569
misteryuan <857951500@qq.com>
2019-01-07 15:46:10 +0800

    添加功能

--- xwx.go
+++ xwx.go
@@ -6,6 +6,7 @@ import (
 	"encoding/xml"
 	"errors"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -81,7 +82,7 @@ func GetInfoAccessToken(o *info, code string) (*getInfoATRes, error) {
 }
 
 // 使用token 获取用户信息
-type profileRes struct {
+type Profile struct {
 	Openid     string   `json:"openid"`
 	Nickname   string   `json:"nickname"`
 	Sex        int      `json:"sex"`
@@ -93,7 +94,7 @@ type profileRes struct {
 	Unionid    string   `json:"unionid"`
 }
 
-func GetProfile(accessToken string, openID string) (*profileRes, error) {
+func GetProfile(accessToken string, openID string) (*Profile, error) {
 	url := fmt.Sprintf("https://api.weixin.qq.com/sns/userinfo?access_token=%s&openid=%s&lang=zh_CN", accessToken, openID)
 	res, err := http.Get(url)
 	if err != nil {
@@ -106,7 +107,7 @@ func GetProfile(accessToken string, openID string) (*profileRes, error) {
 	if err != nil {
 		return nil, err
 	}
-	pres := new(profileRes)
+	pres := new(Profile)
 	return pres, json.Unmarshal(bs, pres)
 }
 
@@ -126,7 +127,86 @@ func SignVerify(token, signature, timestamp, nonce string) bool {
 /*
 微信消息处理
 */
-type getMsgBaser interface {
+//消息类型
+const (
+	MT_EVENT = iota + 1
+	MT_TEXT
+	MT_IMAGE
+	MT_VOICE
+	MT_VIDEO
+	MT_SHORTVIDEO
+	MT_LOCATION
+	MT_LINK
+)
+
+type MT struct {
+	XML     xml.Name `xml:"xml"`
+	MsgType string   `xml:"MsgType"`
+}
+
+func getMT(data []byte) int {
+	v := new(MT)
+	if err := xml.Unmarshal(data, v); err != nil {
+		log.Panicln(err)
+	}
+	switch v.MsgType {
+	case "event":
+		return MT_EVENT
+	case "text":
+		return MT_TEXT
+	case "image":
+		return MT_IMAGE
+	case "voice":
+		return MT_VOICE
+	case "video":
+		return MT_VIDEO
+	case "shortvideo":
+		return MT_SHORTVIDEO
+	case "location":
+		return MT_LOCATION
+	case "link":
+		return MT_LINK
+	}
+	panic("不能识别的微信事件推送")
+}
+
+const (
+	E_SUBSCRIBE   = iota + 1 //关注
+	E_UNSUBSCRIBE            //取消关注
+	E_SCAN                   //用户已关注时的事件推送
+	E_LOCATION               //上报地理位置事件
+	E_CLICK                  //自定义菜单事件
+	E_VIEW                   //点击菜单跳转链接时的事件推送
+)
+
+type ET struct {
+	XML    xml.Name `xml:"xml"`
+	Eevent string   `xml:"Event"`
+}
+
+func getET(data []byte) int {
+	v := new(ET)
+	if err := xml.Unmarshal(data, v); err != nil {
+		log.Panicln(err)
+	}
+	switch v.Eevent {
+	case "subscribe":
+		return E_SUBSCRIBE
+	case "unsubscribe":
+		return E_UNSUBSCRIBE
+	case "SCAN":
+		return E_SCAN
+	case "LOCATION":
+		return E_LOCATION
+	case "CLICK":
+		return E_CLICK
+	case "VIEW":
+		return E_VIEW
+	}
+	panic("不能识别的事件类型")
+}
+
+type GetMsgBaser interface {
 	getMsgBase() *msgBase
 }
 
@@ -135,25 +215,87 @@ type msgBase struct {
 	ToUserName   string   `xml:"ToUserName"`
 	FromUserName string   `xml:"FromUserName"`
 	CreateTime   string   `xml:"CreateTime"`
-	MsgType      string   `xml:"MsgType"`
-	MsgId        string   `xml:"MsgId"`
+	MT
 }
 
-func (m *msgBase) getMsgBase() *msgBase {
-	return m
+func (m msgBase) getMsgBase() *msgBase {
+	return &m
 }
 
-type textMsgR struct {
-	*msgBase
-	Content string `xml:"Content"`
+type eventBase struct {
+	msgBase
+	Event string `xml:"Event" text:"事件类型"`
 }
 
-func GetTextMsg(data []byte) *textMsgR {
-	v := new(textMsgR)
+// 1. 用户未关注时，进行关注后的事件推送,2. 用户已关注时的事件推送
+type SubEvent struct {
+	eventBase
+	EventKey string `xml:"EventKey" text:"事件KEY值，qrscene_为前缀，后面为二维码的参数值"`
+	Ticket   string `xml:"Ticket" text:"二维码的ticket，可用来换取二维码图片"`
+}
+
+func getSubEvent(data []byte) *SubEvent {
+	v := new(SubEvent)
 	if err := xml.Unmarshal(data, v); err != nil {
-		log.Fatal(err)
+		log.Panicln(err)
 	}
 	return v
+}
+
+// 取消关注事件
+type UnSubEvent struct {
+	eventBase
+}
+
+func getUnSubEvent(data []byte) *UnSubEvent {
+	v := new(UnSubEvent)
+	if err := xml.Unmarshal(data, v); err != nil {
+		log.Panicln(err)
+	}
+	return v
+}
+
+type TextMsg struct {
+	msgBase
+	Content string `xml:"Content"`
+	MsgId   string `xml:"MsgId"`
+}
+
+func getTextMsg(data []byte) *TextMsg {
+	v := new(TextMsg)
+	if err := xml.Unmarshal(data, v); err != nil {
+		log.Panicln(err)
+	}
+	return v
+}
+
+/*
+解析微信消息类型和消息体
+返回消息类型和消息体
+*/
+func GetMsg(rc io.ReadCloser) (interface{}, int, int) {
+	data, err := ioutil.ReadAll(rc)
+	if err != nil {
+		panic(err.Error())
+	}
+	defer rc.Close()
+	switch getMT(data) {
+	case MT_TEXT:
+		return getTextMsg(data), MT_TEXT, 0
+	case MT_EVENT:
+		switch getET(data) {
+		case E_SUBSCRIBE:
+			return getSubEvent(data), MT_EVENT, E_SUBSCRIBE
+		case E_UNSUBSCRIBE:
+			return getUnSubEvent(data), MT_EVENT, E_UNSUBSCRIBE
+		default:
+			log.Println("待开发的事件类型")
+			return nil, 0, 0
+		}
+	default:
+		log.Println("待开发的消息类型")
+		return nil, 0, 0
+	}
 }
 
 /*响应消息数据格式*/
@@ -179,7 +321,7 @@ type textReply struct {
 	Content CDATA
 }
 
-func GetTextReply(mber getMsgBaser, text string) *textReply {
+func GetTextReply(mber GetMsgBaser, text string) *textReply {
 	mb := mber.getMsgBase()
 	tr := new(textReply)
 	tr.ToUserName = newCDATA(mb.FromUserName)
